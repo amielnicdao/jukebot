c39ea4d9140bc0968d2b91d946782a672b2d82ee
anicdao <amielnicdao@gmail.com>
2019-11-11 22:50:38 -0500

    added switch statement and log.txt

--- liri.js
+++ liri.js
@@ -1,5 +1,32 @@
 require("dotenv").config();
-
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
 var keys = require("./keys.js");
+var fs = require("fs");
+var moment = require("moment");
 
-var spotify = new Spotify(keys.spotify);
+var nodeArgs = process.argv[2];
+var userInput = process.argv.slice(3).join(" ");
+
+  switch (nodeArgs) {
+    case "spotify-this-song":
+      spotify(userInput);
+      break;
+
+    case "concert-this":
+      bandsInTown(userInput);
+      break;
+
+    case "movie-this":
+      omdb(userInput);
+      break;
+
+    case "do-what-it-says":
+      random(userInput);
+      break;
+
+    default:
+      console.log("Try one of these commands: 'concert-this', 'spotify-this-song', 'movie-this', or 'do-what-it-says'.")
+    }
+
+ 
