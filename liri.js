faeb19c73568b9e5dd89170d68d37262483b1e9f
anicdao <amielnicdao@gmail.com>
2019-11-12 15:09:55 -0500

    added function for omdb

--- liri.js
+++ liri.js
@@ -35,7 +35,7 @@ function bandsInTown(artist) {
   var artist = userInput;
 
   axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-
+ 
     .then(function (response) {
       console.log("Venue: " + response.data[0].venue.name);
       console.log("City: " + response.data[0].venue.city);
@@ -52,6 +52,7 @@ function spotify(song) {
   };
 
   spotify.search({type: "track", query: song}, function(err, data){
+
     if(err) {
       return console.log("Error occurred: " + err);
     }
@@ -60,5 +61,26 @@ function spotify(song) {
     console.log("Song: " + data.tracks.items[0].name);
     console.log("Preview: " + data.tracks.items[0].href);
     console.log("Album: " + data.tracks.items[0].album.name);
+  });
+}
+
+function omdb(movie) {
+
+  if(!movie) {
+    movie = "Mr. Nobody";
+  }
+
+  var movieQueryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+
+  axios.request(movieQueryURL).then(function (response) {
+
+    console.log("Title: " + response.data.Title);
+    console.log("Year Released: " + response.data.Year);
+    console.log("IMDB Rating: " + response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+    console.log("Country Where Produced: " + response.data.Country);
+    console.log("Movie Language: " + response.data.Language);
+    console.log("Plot: " + response.data.Plot);
+    console.log("Actors: " + response.data.Actors);
   });
 }
